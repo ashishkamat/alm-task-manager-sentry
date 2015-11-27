7109bf403b21f7dbefef56244a1a7d70a3e3cdf6
Ashish <ashishka@cybage.com>
2015-11-27 15:10:14 +0530

    ATM-26 Added perl-style comments to a file

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -9,9 +9,9 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	 */
 	public function createApplication()
 	{
-		$unitTesting = true;
+		$unitTesting = true; # Define variable
 
-		$testEnvironment = 'testing';
+		$testEnvironment = 'testing'; # Passing value to variable
 
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
