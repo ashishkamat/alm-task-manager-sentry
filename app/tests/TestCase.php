2c94c73521336290b5c13caed96d75b6f0ae5137
Ashish <ashishka@cybage.com>
2015-11-27 15:14:38 +0530

    ATM-26 Code Duplication to a file

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -15,5 +15,12 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
+	public function createApplication()
+	{
+		$unitTesting = true; # Define variable
 
+		$testEnvironment = 'testing'; # Passing value to variable
+
+		return require __DIR__.'/../../bootstrap/start.php';
+	}
 }
